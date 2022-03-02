1ff073392491a951acea411046bb0e3c786f0ae1
rouge9 <99325915+rouge9@users.noreply.github.com>
2022-03-02 10:59:09 -0800

    Add files via upload

--- timmy_turtle.py
+++ timmy_turtle.py
@@ -1,15 +1,13 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
+
+for _ in range(4):
+    tim.forward(100)
+    tim.left(90)
+
 
 
 
