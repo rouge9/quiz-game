0030b23c9713de16c8fc376876e453a20e92d6be
rouge9 <99325915+rouge9@users.noreply.github.com>
2022-03-02 11:50:33 -0800

    Add files via upload

--- timmy_turtle.py
+++ timmy_turtle.py
@@ -1,14 +1,20 @@
+import random
 from turtle import Turtle, Screen
 
 tim = Turtle()
 
+def draw_side(num_side):
+    angle = 360/num_side
+    for _ in range(num_side):
+        tim.forward(100)
+        tim.right(angle)
 
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+for shape_side in range(3,11):
+    tim.color(random.choice(colours))
+    draw_side(shape_side)
+
 
 screen = Screen()
 screen.exitonclick()
