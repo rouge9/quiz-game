61af8b3917ef1653e22850a56d16aed11de57305
rouge9 <99325915+rouge9@users.noreply.github.com>
2022-03-02 11:08:19 -0800

    Add files via upload
    dotted lines

--- timmy_turtle.py
+++ timmy_turtle.py
@@ -1,18 +1,14 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
-tim.shape("turtle")
-tim.color("red")
-
-for _ in range(4):
-    tim.forward(100)
-    tim.left(90)
 
 
 
-
-
-
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
 screen = Screen()
 screen.exitonclick()
