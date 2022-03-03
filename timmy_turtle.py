8d2fe74b7a3bcfdc5fa6c2bbd0b6a1211788cbf3
rouge9 <99325915+rouge9@users.noreply.github.com>
2022-03-02 23:26:19 -0800

    Add files via upload
    random colors

--- timmy_turtle.py
+++ timmy_turtle.py
@@ -1,17 +1,26 @@
 import random
+import turtle
 from turtle import Turtle, Screen
 
 tim = Turtle()
+turtle.colormode(255)
 
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color_tuple = (r,g,b)
+    return color_tuple
 
 direction = [0,90,180,270]
 tim.width(15)
 tim.speed("fastest")
-for d in range(1000):
-    tim.color(random.choice(colours))
-    tim.forward(70)
-    tim.setheading(random.choice(direction))
+
+# for d in range(1000):
+#     tim.color(random_color())
+#     tim.forward(70)
+#     tim.setheading(random.choice(direction))
 
 
 screen = Screen()
