2923c02521b773b1c8070e812dd461de7d94f0f6
rouge9 <99325915+rouge9@users.noreply.github.com>
2022-03-05 22:54:28 -0800

    added second paddle

--- pong_main.py
+++ pong_main.py
@@ -1,4 +1,3 @@
-
 from turtle import Turtle, Screen
 import time
 from paddle import Paddle
@@ -11,14 +10,16 @@ screen.setup(800, 600)
 screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0)
-paddle_1 = Paddle()
-
+paddle_1 = Paddle((350, 0))
+paddle_2 = Paddle((-350, 0))
 time.sleep(0.1)
 
 screen.listen()
 
 screen.onkey(paddle_1.move_up, "Up")
 screen.onkey(paddle_1.move_down, "Down")
+screen.onkey(paddle_2.move_up, "w")
+screen.onkey(paddle_2.move_down, "s")
 
 while is_game_on:
     screen.update()
