af5d426c964ec22d963723ea75c57a230818e46b
rouge9 <99325915+rouge9@users.noreply.github.com>
2022-03-07 01:50:07 -0800

    Turtle crossing game

---
+++ turtle_crossing_main.py
@@ -0,0 +1,37 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard_for_turtle_crossing import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+player = Player()
+car = CarManager()
+score = Scoreboard()
+screen.listen()
+screen.onkey(player.go_forward, "Up")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car.create_car()
+    car.move_car()
+
+    # Detect collusion with the car
+    for single in car.all_cars:
+        if single.distance(player) < 20:
+            score.game_over()
+            game_is_on = False
+
+    if player.is_at_finish_line():
+        player.go_to_starting_line()
+        car.level_up()
+        score.count()
+
+
+
+screen.exitonclick()
