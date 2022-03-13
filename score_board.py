48aefe9409dcfef0871eeef4d81ef6e96468a684
rouge9 <99325915+rouge9@users.noreply.github.com>
2022-03-13 01:08:58 -0800

    added a dtat.txt file

--- score_board.py
+++ score_board.py
@@ -11,12 +11,24 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 280)
         self.hideturtle()
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.update
 
     def update(self):
-        self.write(f"scoreboard: {self.score}", align=AlIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"scoreboard: {self.score}     High Score {self.high_score}", align=AlIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update()
 
     def increase_scoreboard(self):
         self.score += 1
         self.clear()
         self.update()
+
